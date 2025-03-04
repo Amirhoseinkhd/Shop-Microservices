358e19a48b51170e3b320c4445b2609c740702f0
Amirhoseinkhd <iamir.kh98@gmail.com>
2025-03-04 17:28:39 +0330

    Fixed

--- src/WebApps/Shopping.Web/Services/IOrderingService.cs
+++ src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -2,7 +2,7 @@ namespace Shopping.Web.Services;
 
 public interface IOrderingService
 {
-    [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}\"")]
+    [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}")]
     Task<GetOrdersResponse> GetOrders(int? pageIndex = 1, int? pageSize = 10);
 
     [Get("/ordering-service/orders/{orderName}")]
